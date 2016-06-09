2f4686485116439b67bcb07d074f55e11b1dbb9e
nurulizzah <izzaharah@gmail.com>
2016-06-09 09:44:13 +0800

    ssh client

---
+++ socket/sshc.py
@@ -0,0 +1,14 @@
+import paramiko 
+hostname='127.0.0.1'
+port=22
+username='user'
+pkey_file='/home/niaa/python/socket/.ssh/id.rsa'
+
+if __name__ == "__main__":
+	key = paramiko.RSAKey.from_private_key_file(pkey_file)
+	s = paramiko.SSHClient()
+	s.load_system_host_keys()
+	s.connect(hostname,port,pkey=key)
+	stdin, stdout, stderr=s.exec_command('ifconfig')
+	print stdout.read()
+	s.close()
